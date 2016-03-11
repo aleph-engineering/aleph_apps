28b2db3d507134e4c9e574ebe5581a4e883775c0
reFucktor <hmvalcarcel@gmail.com>
2016-03-11 13:25:07 -0500

    Include routes for cuban engineer app.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var cubaneng = require('./routes/ce_routes');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use('/', routes);
+app.use('/ce', cubaneng);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
