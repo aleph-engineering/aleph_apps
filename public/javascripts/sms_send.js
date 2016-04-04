613528d70a654a0bd539e1a42b650fdea087e0f5
reFucktor <hmvalcarcel@gmail.com>
2016-04-04 16:08:15 -0400

    Fixing sms script and set timeout for ajax request.

--- public/javascripts/sms_send.js
+++ public/javascripts/sms_send.js
@@ -11,19 +11,20 @@ function includeNumber(){
         $('#phone_number').attr("value", parseInt(numbers));
     })
 }
-function sendSMS(){
-    $('#send').click(function(){
+function sendSMS() {
+    $('#send').click(function () {
         var number = $('#phone_number').val();
         var message = $('#body_message').val();
         var data = {number: '+' + number, message: message};
         $.ajax({
             url: '/api/sendSMS',
             dataType: "json",
-            data: data
-        }).done(function(data){
+            data: data,
+            timeout: 10000
+        }).done(function (data) {
             Materialize.toast("Your message was sent correctly", 2000);
-        }).fail(function(data){
-            Materialize.toast("Error! Your message couldn't be sent. Message: "+ data, 2000);
+        }).fail(function (data) {
+            Materialize.toast("Error! Your message couldn't be sent. Message: " + data.responseText, 2000);
         });
     });
 }
