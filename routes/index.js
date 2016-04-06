e775239385d244aa0e1cb2d21e5b81ad4adaaf46
reFucktor <hmvalcarcel@gmail.com>
2016-04-06 08:12:46 -0400

    toi: Set routes for login and locally register, include login link for fb,lIn,g+ in views.

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@
 var express = require('express');
 var router = express.Router();
 var LocalUser = require('../app/models');
+var userController = require('../app/controllers/userProfile');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    if(req.user)
     res.render('login', { title: "[ae] Apps", user : req.user, message : req.flash('error')});
 });
 router.get('/login', function(req, res, next) {
