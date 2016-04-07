c430891dbee4bb80495d32bb2a29bb85b44e3dcd
reFucktor <hmvalcarcel@gmail.com>
2016-04-07 12:34:08 -0400

    toi: User serialization for passport module.

--- app/passport/index.js
+++ app/passport/index.js
@@ -7,14 +7,19 @@ const passportConfig = function(app){
     app.use(passport.initialize());
     app.use(passport.session());
 
-    passport.serializeUser(localUser.serializeUser());
+    /*passport.serializeUser(localUser.serializeUser());
     passport.deserializeUser(localUser.deserializeUser());
-    /*passport.serializeUser(function(user, done){
-        done(null, user);
-    });
     passport.deserializeUser(function(user, done){
         done(null, user);
     });*/
+    passport.serializeUser(function(user, done) {
+        done(null, user._id);
+    });
+    passport.deserializeUser(function(id, done) {
+        localUser.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
 
     require('./local')(app);
     require('./facebook')(app);
