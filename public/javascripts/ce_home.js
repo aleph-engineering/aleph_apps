a433e43f0729847ec10647cab337578abd48be52
reFucktor <hmvalcarcel@gmail.com>
2016-04-12 01:48:26 -0400

    Finish terminal in home page.

--- public/javascripts/ce_home.js
+++ public/javascripts/ce_home.js
@@ -8,18 +8,32 @@ function activatePlay(){
     $('.terminal-input').keypress(function(event) {
         if (event.keyCode == 13) {
             var word = $(this).val();
-            $(this).val("");
-            if(word  === 'play'){
-                $('.panel-container').addClass('animated zoomOut');
+            var terminal = $(this);
+            var formContainer = $('.panel-container');
+            var videoContainer = $('.video-container');
+            var secondLine = $('.second-line');
+            var zoomOutClass = "animated zoomOut";
+            var zoomInClass = "animated zoomIn";
+            var hidedClass = "hided";
+            var playComment = "# type 'play' to see more";
+            var returnComment = "# type 'return' to see the form";
+            $(terminal).val("");
+            if(word  === 'play' && $(videoContainer).hasClass(hidedClass)){
+                $(formContainer).addClass(zoomOutClass);
+                $(secondLine).text(returnComment);
                 window.setTimeout(function(){
-                    $('.video-container').addClass('animated zoomIn');
-                    $('.panel-container').addClass('hided');
+                    $(videoContainer).removeClass(hidedClass).addClass(zoomInClass);
+                    $(formContainer).addClass(hidedClass);
+                    $(formContainer).removeClass(zoomOutClass);
                 },1000);
             }
-            else if(word === 'fuck'){
-                var previousText = $('.second-line').text();
-                $('.first-line').text(previousText);
-                $('.second-line').text("root@nauta~:# " + word);
+            else if(word === 'return' && $(formContainer).hasClass(hidedClass)){
+                $(videoContainer).removeClass(zoomInClass).addClass(zoomOutClass);
+                $(secondLine).text(playComment);
+                window.setTimeout(function(){
+                    $(formContainer).removeClass(hidedClass).addClass(zoomInClass);
+                    $(videoContainer).addClass(hidedClass).removeClass(zoomOutClass);
+                },1000);
             }
         }
     });
